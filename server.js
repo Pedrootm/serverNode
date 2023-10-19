115182a89a510fdc4942c1bf27c65126ccf49da0
Pedro Oliveira <37086283+Pedrootm@users.noreply.github.com>
2023-10-19 02:10:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -21,27 +21,9 @@ app.get('/search', async (req, res) => {
     }
 
     // Faz a solicitação para a página web com base no URL fornecido
-    const response = await axios.get(`https://www.anitube.vip/busca.php?s=${keyword}&submit=Buscar`)
+    const response = await axios.get(`https://www.anitube.vip/busca.php?s=${keyword}&submit=Buscar`);
 console.log(response);
 
-.catch(function (error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-      // http.ClientRequest in node.js
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log('Error', error.message);
-    }
-    console.log(error.config);
-  });      
       
   
       
