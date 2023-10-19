2a454786f843da167ac05b966bbb4422d1ef65a8
Pedro Oliveira <37086283+Pedrootm@users.noreply.github.com>
2023-10-18 23:41:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/search', async (req, res) => {
 
       const errorHandling = ()  => {
     // Faz a solicitação para a página web com base no URL fornecido
-    const response = await axios.get(`https://www.anitube.vip/busca.php?s=${keyword}&submit=Buscar`);
+    const response = axios.get(`https://www.anitube.vip/busca.php?s=${keyword}&submit=Buscar`);
 
 
 .catch(function (error) {
