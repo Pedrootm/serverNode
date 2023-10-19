a65475cad3542180382ea4207b6aaec2cf4d9155
Pedro Oliveira <37086283+Pedrootm@users.noreply.github.com>
2023-10-18 23:30:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -23,6 +23,35 @@ app.get('/search', async (req, res) => {
     // Faz a solicitação para a página web com base no URL fornecido
     const response = await axios.get(`https://www.anitube.vip/busca.php?s=${keyword}&submit=Buscar`);
 
+
+
+
+
+.catch(function (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
+  });      
+
+
+
+
+
+
+      
     // Carrega o conteúdo HTML da página
     const $ = cheerio.load(response.data);
 
